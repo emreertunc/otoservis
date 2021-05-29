cace70bde648edc1580d5afc6a4d9e7fef8efcce
LAPTOP-AJTDFNSV\ertun <ertuncemre@gmail.com>
2021-05-29 20:08:46 +0300

    gereksiz sayfalar temizlendi

--- Proje.Web/Sablon.Master.designer.cs
+++ Proje.Web/Sablon.Master.designer.cs
@@ -238,14 +238,5 @@ namespace Proje.Web
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        /// <summary>
-        /// silmeonaybutton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlButton silmeonaybutton;
     }
 }
